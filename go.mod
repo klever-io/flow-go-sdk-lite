c5423bc76a32e4344c9b0081c7b974474fa89b47
sibelly <sibellychan@hotmail.com>
2023-02-21 17:49:19 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,62 @@
+module github.com/klever-io/flow-go-sdk-lite
+
+go 1.19
+
+require (
+	cloud.google.com/go/kms v1.4.0
+	github.com/ethereum/go-ethereum v1.9.13
+	github.com/onflow/cadence v0.34.0
+	github.com/onflow/flow-go-sdk v0.33.0
+	github.com/onflow/flow-go/crypto v0.24.4
+	github.com/onflow/flow/protobuf/go/flow v0.3.1
+	github.com/onflow/sdks v0.5.0
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.7.5
+	google.golang.org/api v0.70.0
+	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
+)
+
+require (
+	cloud.google.com/go v0.100.2 // indirect
+	cloud.google.com/go/compute v1.3.0 // indirect
+	cloud.google.com/go/iam v0.1.0 // indirect
+	github.com/bits-and-blooms/bitset v1.2.2 // indirect
+	github.com/btcsuite/btcd v0.20.1-beta // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fxamacker/cbor/v2 v2.4.1-0.20220515183430-ad2eae63303f // indirect
+	github.com/fxamacker/circlehash v0.3.0 // indirect
+	github.com/go-test/deep v1.0.5 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.14 // indirect
+	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381 // indirect
+	github.com/onflow/atree v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.1-0.20211004051800-57c86be7915a // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/texttheater/golang-levenshtein/levenshtein v0.0.0-20200805054039-cae8b0eaed6c // indirect
+	github.com/turbolent/prettier v0.0.0-20220320183459-661cc755135d // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	github.com/zeebo/blake3 v0.2.3 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	go.opentelemetry.io/otel v1.8.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+replace (
+	github.com/onflow/cadence => /Users/sibellysanches/Documents/projects/github/klever/cadence-go-sdk-lite
+	github.com/onflow/flow-go-sdk => /Users/sibellysanches/Documents/projects/github/klever/flow-go-sdk-lite
+
+)
---
+++ test/entities.go
@@ -0,0 +1,436 @@
+/*
+ * Flow Go SDK
+ *
+ * Copyright 2019 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package test
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/onflow/cadence"
+	jsoncdc "github.com/onflow/cadence/encoding/json"
+	"github.com/onflow/cadence/runtime/common"
+
+	"github.com/onflow/flow-go-sdk"
+	"github.com/onflow/flow-go-sdk/crypto"
+)
+
+type Accounts struct {
+	addresses   *Addresses
+	accountKeys *AccountKeys
+}
+
+func AccountGenerator() *Accounts {
+	return &Accounts{
+		addresses:   AddressGenerator(),
+		accountKeys: AccountKeyGenerator(),
+	}
+}
+
+func (g *Accounts) New() *flow.Account {
+	return &flow.Account{
+		// Address: g.addresses.New(),
+		Balance: 10,
+		Keys: []*flow.AccountKey{
+			g.accountKeys.New(),
+			g.accountKeys.New(),
+		},
+	}
+}
+
+type AccountKeys struct {
+	count int
+	ids   *Identifiers
+}
+
+func AccountKeyGenerator() *AccountKeys {
+	return &AccountKeys{
+		count: 1,
+		ids:   IdentifierGenerator(),
+	}
+}
+
+func (g *AccountKeys) New() *flow.AccountKey {
+	accountKey, _ := g.NewWithSigner()
+	return accountKey
+}
+
+func (g *AccountKeys) NewWithSigner() (*flow.AccountKey, crypto.Signer) {
+	defer func() { g.count++ }()
+
+	seed := make([]byte, crypto.MinSeedLength)
+	for i := range seed {
+		seed[i] = uint8(g.count)
+	}
+
+	privateKey, err := crypto.GeneratePrivateKey(crypto.ECDSA_P256, seed)
+
+	if err != nil {
+		panic(err)
+	}
+
+	accountKey := flow.AccountKey{
+		Index:          g.count,
+		PublicKey:      privateKey.PublicKey(),
+		SigAlgo:        crypto.ECDSA_P256,
+		HashAlgo:       crypto.SHA3_256,
+		Weight:         flow.AccountKeyWeightThreshold,
+		SequenceNumber: 42,
+	}
+
+	// error here is nil since ECDSA_P256 and SHA3_256 are compatible,
+	// but keeping the error check for sanity
+	signer, err := crypto.NewInMemorySigner(privateKey, accountKey.HashAlgo)
+	if err != nil {
+		panic(err)
+	}
+
+	return &accountKey, signer
+}
+
+type Addresses struct {
+	generator *flow.AddressGenerator
+}
+
+func AddressGenerator() *Addresses {
+	return &Addresses{
+		generator: flow.NewAddressGenerator(flow.Emulator),
+	}
+}
+
+// func (g *Addresses) New() flow.Address {
+// 	return g.generator.NextAddress()
+// }
+
+type Blocks struct {
+	headers    *BlockHeaders
+	guarantees *CollectionGuarantees
+	seals      *BlockSeals
+	signatures *Signatures
+}
+
+func BlockGenerator() *Blocks {
+	return &Blocks{
+		headers:    BlockHeaderGenerator(),
+		guarantees: CollectionGuaranteeGenerator(),
+		seals:      BlockSealGenerator(),
+		signatures: SignaturesGenerator(),
+	}
+}
+
+func (g *Blocks) New() *flow.Block {
+	header := g.headers.New()
+
+	guarantees := []*flow.CollectionGuarantee{
+		g.guarantees.New(),
+		g.guarantees.New(),
+		g.guarantees.New(),
+	}
+
+	seals := []*flow.BlockSeal{
+		g.seals.New(),
+	}
+
+	payload := flow.BlockPayload{
+		CollectionGuarantees: guarantees,
+		Seals:                seals,
+	}
+
+	return &flow.Block{
+		BlockHeader:  header,
+		BlockPayload: payload,
+	}
+}
+
+type BlockHeaders struct {
+	count     int
+	ids       *Identifiers
+	startTime time.Time
+}
+
+func BlockHeaderGenerator() *BlockHeaders {
+	startTime, _ := time.Parse(time.RFC3339, "2020-06-04T15:43:21+00:00")
+
+	return &BlockHeaders{
+		count:     1,
+		ids:       IdentifierGenerator(),
+		startTime: startTime.UTC(),
+	}
+}
+
+func (g *BlockHeaders) New() flow.BlockHeader {
+	defer func() { g.count++ }()
+
+	return flow.BlockHeader{
+		ID:        g.ids.New(),
+		ParentID:  g.ids.New(),
+		Height:    uint64(g.count),
+		Timestamp: g.startTime.Add(time.Hour * time.Duration(g.count)),
+	}
+}
+
+type Collections struct {
+	ids *Identifiers
+}
+
+func CollectionGenerator() *Collections {
+	return &Collections{
+		ids: IdentifierGenerator(),
+	}
+}
+
+func (g *Collections) New() *flow.Collection {
+	return &flow.Collection{
+		TransactionIDs: []flow.Identifier{
+			g.ids.New(),
+			g.ids.New(),
+		},
+	}
+}
+
+type CollectionGuarantees struct {
+	ids *Identifiers
+}
+
+type BlockSeals struct {
+	ids *Identifiers
+}
+
+func CollectionGuaranteeGenerator() *CollectionGuarantees {
+	return &CollectionGuarantees{
+		ids: IdentifierGenerator(),
+	}
+}
+
+func (g *CollectionGuarantees) New() *flow.CollectionGuarantee {
+	return &flow.CollectionGuarantee{
+		CollectionID: g.ids.New(),
+	}
+}
+
+func BlockSealGenerator() *BlockSeals {
+	return &BlockSeals{
+		ids: IdentifierGenerator(),
+	}
+}
+
+func (g *BlockSeals) New() *flow.BlockSeal {
+	return &flow.BlockSeal{
+		BlockID:            g.ids.New(),
+		ExecutionReceiptID: g.ids.New(),
+	}
+}
+
+type Events struct {
+	count int
+	ids   *Identifiers
+}
+
+func EventGenerator() *Events {
+	return &Events{
+		count: 1,
+		ids:   IdentifierGenerator(),
+	}
+}
+
+func (g *Events) New() flow.Event {
+	defer func() { g.count++ }()
+
+	identifier := fmt.Sprintf("FooEvent%d", g.count)
+
+	location := common.StringLocation("test")
+
+	testEventType := &cadence.EventType{
+		Location:            location,
+		QualifiedIdentifier: identifier,
+		Fields: []cadence.Field{
+			{
+				Identifier: "a",
+				Type:       cadence.IntType{},
+			},
+			{
+				Identifier: "b",
+				Type:       cadence.StringType{},
+			},
+		},
+	}
+
+	testEvent := cadence.NewEvent(
+		[]cadence.Value{
+			cadence.NewInt(g.count),
+			cadence.String("foo"),
+		}).WithType(testEventType)
+
+	typeID := location.TypeID(nil, identifier)
+
+	payload, err := jsoncdc.Encode(testEvent)
+	if err != nil {
+		panic(fmt.Errorf("cannot encode test event: %w", err))
+	}
+
+	event := flow.Event{
+		Type:             string(typeID),
+		TransactionID:    g.ids.New(),
+		TransactionIndex: g.count,
+		EventIndex:       g.count,
+		Value:            testEvent,
+		Payload:          payload,
+	}
+
+	return event
+}
+
+type Signatures struct {
+	count int
+}
+
+func SignaturesGenerator() *Signatures {
+	return &Signatures{1}
+}
+
+func (g *Signatures) New() [][]byte {
+	defer func() { g.count++ }()
+	return [][]byte{
+		[]byte(strconv.Itoa(g.count + 1)),
+	}
+}
+
+func newSignatures(count int) Signatures {
+	return Signatures{
+		count: count,
+	}
+}
+
+type Identifiers struct {
+	count int
+}
+
+func IdentifierGenerator() *Identifiers {
+	return &Identifiers{1}
+}
+
+func (g *Identifiers) New() flow.Identifier {
+	defer func() { g.count++ }()
+	return newIdentifier(g.count + 1)
+}
+
+func newIdentifier(count int) flow.Identifier {
+	var id flow.Identifier
+	for i := range id {
+		id[i] = uint8(count)
+	}
+
+	return id
+}
+
+type Transactions struct {
+	count     int
+	greetings *Greetings
+}
+
+func TransactionGenerator() *Transactions {
+	return &Transactions{
+		count:     1,
+		greetings: GreetingGenerator(),
+	}
+}
+
+func (g *Transactions) New() *flow.Transaction {
+	tx := g.NewUnsigned()
+
+	// sign payload with proposal key
+	err := tx.SignPayload(
+		tx.ProposalKey.Address,
+		tx.ProposalKey.KeyIndex,
+		MockSigner([]byte{uint8(tx.ProposalKey.KeyIndex)}),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// sign payload as each authorizer
+	for _, addr := range tx.Authorizers {
+		err = tx.SignPayload(addr, 0, MockSigner(addr.Bytes()))
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// sign envelope as payer
+	err = tx.SignEnvelope(tx.Payer, 0, MockSigner(tx.Payer.Bytes()))
+	if err != nil {
+		panic(err)
+	}
+
+	return tx
+}
+
+func (g *Transactions) NewUnsigned() *flow.Transaction {
+	blockID := newIdentifier(g.count + 1)
+
+	accounts := AccountGenerator()
+	accountA := accounts.New()
+	accountB := accounts.New()
+
+	proposalKey := accountA.Keys[0]
+
+	tx := flow.NewTransaction().
+		SetScript(GreetingScript).
+		SetReferenceBlockID(blockID).
+		SetGasLimit(42).
+		SetProposalKey(accountA.Address, proposalKey.Index, proposalKey.SequenceNumber).
+		AddAuthorizer(accountA.Address).
+		SetPayer(accountB.Address)
+
+	err := tx.AddArgument(cadence.String(g.greetings.New()))
+	if err != nil {
+		panic(err)
+	}
+
+	return tx
+}
+
+type TransactionResults struct {
+	events *Events
+}
+
+func TransactionResultGenerator() *TransactionResults {
+	return &TransactionResults{
+		events: EventGenerator(),
+	}
+}
+
+func (g *TransactionResults) New() flow.TransactionResult {
+	eventA := g.events.New()
+	eventB := g.events.New()
+	blockID := newIdentifier(1)
+	blockHeight := uint64(42)
+
+	return flow.TransactionResult{
+		Status: flow.TransactionStatusSealed,
+		Error:  errors.New("transaction execution error"),
+		Events: []flow.Event{
+			eventA,
+			eventB,
+		},
+		BlockID:     blockID,
+		BlockHeight: blockHeight,
+	}
+}
